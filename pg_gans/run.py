0a9e8d14c47b75777759d0bd8cad29f26e51c247
gregoire <gregoire@plumelabs.com>
2020-08-07 12:44:29 +0200

    fixes

--- pg_gans/run.py
+++ pg_gans/run.py
@@ -78,6 +78,8 @@ if __name__=='__main__':
 
     # Set TF session
     tf.InteractiveSession()
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     # Load model
     type_ = 'face'
--- stylegan2/run.py
+++ stylegan2/run.py
@@ -3,7 +3,12 @@ import pickle
 import dnnlib
 import dnnlib.tflib as tflib
 import numpy as np
+import os
+import PIL
+import tensorflow as tf
 
+
+BATCH_SIZE = 5
 
 TYPE_MODEL = {
     'church': 'stylegan2-church-config-f.pkl',
@@ -30,11 +35,18 @@ def generate_images(generator, discriminator, number, path_out, vectors=None):
 
     # Generate images one by one
     images, probabilities = None, None
-    noise_vars = [var for name, var in g.components.synthesis.vars.items() if name.startswith('noise')]
-    for e in range(number):
+    noise_vars = [var for name, var in generator.components.synthesis.vars.items() if name.startswith('noise')]
+    for i in range(number // BATCH_SIZE):
+        print(f'Batch {i}...')
         tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
-        images = np.concatenate((images, g.run(latents, None)))
-        # TODO compute probabilities
+
+        imgs = generator.run(vectors[i*BATCH_SIZE:(i + 1)*BATCH_SIZE], None)
+        probas = discriminator.run(imgs, None).reshape(-1)
+        if images is None:
+            images, probabilities = imgs, probas
+        else:
+            images = np.concatenate((images, imgs))
+            probabilities = np.concatenate((probabilities, probas))
 
     # Convert images to PIL-compatible format
     images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8) # [-1,1] => [0,255]
@@ -62,6 +74,7 @@ def write_pngs(path_in):
     # Write all pngs
     images = dump['images']
     for i in range(images.shape[0]):
+        print(f'Building image {i}...')
         PIL.Image.fromarray(images[i], 'RGB').save(f'{path_in}/images/{i}.png')
 
 
@@ -69,10 +82,14 @@ if __name__=='__main__':
 
     # Set TF session
     tflib.init_tf()
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     # Load model
     type_ = 'face'
     generator, discriminator = load_model('face')
+
+    number = 20
 
 
     ###
@@ -81,3 +98,16 @@ if __name__=='__main__':
 
     generate(generator, discriminator, number, f'outputs/{type_}/random')
     write_pngs(f'outputs/{type_}/random')
+
+
+    ###
+    # Generate all images between two random vectors
+    ###
+
+    vs = np.random.RandomState(123454321).randn(2, *generator.input_shape[1:])
+    v1, v2 = vs[0,:], vs[1,:]
+
+    # Interpolate between both vectors    
+    vectors = np.concatenate([[(1 - i / number) * v1 + (i / number) * v2] for i in range(number)])
+    generate(generator, discriminator, number, f'outputs/{type_}/interpolation', vectors=vectors)
+    write_pngs(f'outputs/{type_}/interpolation')
